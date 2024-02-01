6a0380a02bd7764a2ed67954ebb23e2082658921
francotaind <francotaind00@gmail.com>
2024-02-01 21:54:42 +0300

    symbol count and manipulation, get_spin function

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
+import random
+
+
+
 MAX_LINES = 3
 MAX_STAKE = 1000
 MIN_STAKE = 50
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+             print(column[row], end=" | ")
+            else:
+                print(column[row],)
+
+    #print()
 
 def deposit():
     while True:
@@ -47,9 +90,23 @@ def get_stake():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    stake = get_stake()
+    while True:
+        stake = get_stake()
+        total_bet = stake * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough balance, your current balance is {balance}")
+        else:
+            break
+
+    #stake = get_stake()
     total_bet = stake * lines
     print(balance, lines, stake)
     print(f"you are staking KSH{stake} on {lines} lines. Total bet is {total_bet}")
+
+
+
+    slots = get_spin(ROWS, COLS, symbol_count)
+    print_machine(slots)
 
 main()
